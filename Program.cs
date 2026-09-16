118e0ae09a97347e67890102ef236ae8de2b3881
jeny <whenycatheryne@gmail.com>
2026-09-16 16:55:43 -0300

    isso cria variavel para usar depois

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//lalala isso vai salvar o nome do user numa variavel
+//esse vai exibir uma mensagem de saudação mostrando o nome do usuário
+var nomeusuario = "Lenine";
+Console.Write("Seja Bem Vindo, ");
+Console.WriteLine(nomeusuario);
